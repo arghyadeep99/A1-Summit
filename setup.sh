80ce47adaaf609af1f13c41c9f3350a8dca85335
arghyadeep99 <arghyadeep.d@somaiya.edu>
2020-11-15 16:40:02 +0530

    Added Heroku config

--- setup.sh
+++ setup.sh
@@ -1,7 +1,10 @@
-mkdir -p ~/.streamlit
-
-echo "[server]
-headless = true
-port = $PORT
-enableCORS = false
-" > ~/.streamlit/config.toml
+mkdir -p ~/.streamlit/
+echo "\
+[general]\n\
+email = \"arghyadeep.d@somaiya.edu\"\n\
+" > ~/.streamlit/credentials.toml
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
